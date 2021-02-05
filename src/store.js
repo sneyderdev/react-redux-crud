44f21b6fea48d32d961a6cda919d8ee8189ee23f
Sneyder Barreto <sneyderbarreto@gmail.com>
2021-02-05 11:53:59 -0500

    ADD: Slice for users and add its reducer to the store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import postsReducer from './reducers/postsSlice';
+import usersReducer from './reducers/usersSlice';
 
 const store = configureStore({
   reducer: {
-    posts: postsReducer
+    posts: postsReducer,
+    users: usersReducer,
   },
 });
 
